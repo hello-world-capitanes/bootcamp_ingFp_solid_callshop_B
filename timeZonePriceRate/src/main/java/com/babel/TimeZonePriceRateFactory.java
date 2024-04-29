5b3e861cab06c6797ff7def47f9189a21fd23ef6
Mireya Medalle Merino <mireya.medalle@babelgroup.com>
2024-04-29 14:07:23 +0200

    Timezones

--- timeZonePriceRate/src/main/java/com/babel/TimeZonePriceRateFactory.java
+++ timeZonePriceRate/src/main/java/com/babel/TimeZonePriceRateFactory.java
@@ -6,12 +6,21 @@ import com.helloworld.callshop.rater.rate.factory.ParametersReader;
 import com.helloworld.callshop.rater.rate.factory.RateBuilderException;
 import com.helloworld.callshop.rater.rate.factory.RateFactory;
 
+import java.beans.XMLDecoder;
+
 public class TimeZonePriceRateFactory implements RateFactory {
 
     private static final String DESCRIPTION = "Tarifa horaria (margen sobre en coste de la llamada)";
 
-    private static final String ID = "TMZ";
-    private static final String PERCENT_NAME = "time_zone";
+    private static final String ID = "TIMEZONE";
+    private static final String SLOT1 = "time_zone_morning";
+    private static final String SLOT2 = "time_zone_afternoon";
+    private static final String SLOT3 = "time_zone_night";
+
+    private static final String SLOT1_RATE_NAME = "rateSlot1";
+    private static final String SLOT2_RATE_NAME = "rateSlot2";
+    private static final String SLOT3_RATE_NAME = "rateSlot3";
+
 
     //private final CostCalculator costCalculator = new SimpleCostCalculator();
     @Override
