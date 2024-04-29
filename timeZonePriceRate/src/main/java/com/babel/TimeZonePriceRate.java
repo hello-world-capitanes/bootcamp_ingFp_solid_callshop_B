1c57e46954f9fff7c13dba6f801d455caca6b41a
Mireya Medalle Merino <mireya.medalle@babelgroup.com>
2024-04-29 13:52:00 +0200

    TimeZonePriceRate

--- timeZonePriceRate/src/main/java/com/babel/TimeZonePriceRate.java
+++ timeZonePriceRate/src/main/java/com/babel/TimeZonePriceRate.java
@@ -1,18 +1,36 @@
 package com.babel;
 
+import com.helloworld.callshop.rate.percentualprice.cost.CostCalculator;
 import com.helloworld.callshop.rater.rate.AbstractRate;
 import com.helloworld.callshop.rater.rate.Rate;
 import com.helloworld.callshop.rater.rate.RateableCall;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class TimeZonePriceRate extends AbstractRate implements Rate {
-    public TimeZonePriceRate(String name) {
+
+    private CostCalculator costCalculator;
+
+    private final int marginPercent;
+
+    public TimeZonePriceRate(String name, int marginPercent) {
         super(name);
+        this.marginPercent = marginPercent;
     }
 
     @Override
     public BigDecimal calculatePrice(RateableCall call) {
-        return null;
+        BigDecimal cost = costCalculator.getCost(call);
+        BigDecimal marginPerOne = BigDecimal.valueOf(marginPercent / 100d);
+        return cost.multiply(marginPerOne.add(BigDecimal.ONE)).setScale(6, RoundingMode.HALF_EVEN);
+    }
+
+    @Override
+    public String toString() {
+        return "TimeZonePriceRate{" +
+                "costCalculator=" + costCalculator +
+                ", marginPercent=" + marginPercent +
+                '}';
     }
 }
